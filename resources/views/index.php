f3372a93df1a63f27b3444f8d2429d688d09e806
Salvador Peña <salvadorp74@hotmail.com>
2017-02-11 21:10:25 -0600

    Polyfill added

--- resources/views/index.php
+++ resources/views/index.php
@@ -3,14 +3,16 @@
 	<meta charset="utf-8">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" 
-			integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-      <script 	src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" 
-				integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous">
+		integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+	<script  src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+      <script  src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" 
+               integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous">
 	</script>
 	<script  type="text/javascript" 
-				src="https://maps.googleapis.com/maps/api/js?key=AIzaSyD2zLX_axeURKX_vjZAyKtjt-0dalOlPEM">		
+               src="https://maps.googleapis.com/maps/api/js?key=AIzaSyD2zLX_axeURKX_vjZAyKtjt-0dalOlPEM">		
 	</script>
+      <script src="bower_components/webcomponentsjs/webcomponents-lite.min.js">
+      </script>
 	<link rel="import" href="bower_components/polymer/polymer.html">
 	<link rel="import" href="bower_components/spinner/Spinner.html">
       <link rel="import" href="bower_components/simulador/Simulador.html">
